38352f52bab2c9218ef853fafe414cc1605f4bb5
Caesar454905970 <454905970@qq.com>
2021-11-08 15:52:03 +0800

    完成了登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,41 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
 
-Vue.use(VueRouter)
+Vue.use(Router)
+const router = new Router({
+  routes: [
+    {
+      // 首页自动跳转
+      path: '/',
+      redirect: 'login'
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home
+    }
+  ]
+})
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+// 挂载路由导航守卫
+router.beforeEach((to,from,next)=>{
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  //     next()  放行    next('/login')  强制跳转
+  if(to.path === '/login') return next()
+  //先获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if(!tokenStr) return  next('login')
+  //token存在
+  // 提交token给服务器，判断token是否有效，有效再。。或者 校验一下本地的token值（把初始值放在vuex里面)
+  // 放行
+  next()
 })
 
 export default router
